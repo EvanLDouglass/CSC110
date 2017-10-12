35361268e08c7c61bc2a105ae87fb9902143c213
Evan Douglass <evanldouglass@gmail.com>
2017-10-11 21:37:16 -0700

    Update Land-Calculation.py

--- Starter_Programs/Land-Calculation.py
+++ Starter_Programs/Land-Calculation.py
@@ -1,14 +1,17 @@
 # Evan Douglass
 # Ch.2 Exercsize 3: Land Calculation
-#
+
 # This program is intended to calculate the number of acres on a tract of land
 # from the total square footage.
 
 # input square footage from user
 sq_ft = float(input('What is the total square footage of land?: '))
+
 # input acre to square foot equivalency
 one_acre = 43560.0 #ft
+
 # Convert square feet into acres
 total = sq_ft / one_acre
+
 # Display results
 print('The tract of land is', format(total, ',.2f'), 'acres')
