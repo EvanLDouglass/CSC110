61ef270e346b699b1ea93a1b620812e7b2186def
Evan Douglass <evanldouglass@gmail.com>
2017-10-11 21:39:10 -0700

    Update Miles-per-gallon.py

--- Starter_Programs/Miles-per-gallon.py
+++ Starter_Programs/Miles-per-gallon.py
@@ -1,13 +1,16 @@
 # Evan Douglass
 # Ch 2 Exercises: Miles-per-gallon
-#
+
 # This program calculates miles per gallon for a user's car
 
 # input miles driven
 miles = float(input('How many miles did you drive (no commas)? '))
+
 # input gallons of gas used
 gallons = float(input('How many gallons of gas did you use (no commas)? '))
+
 # calculate miles per gallon
 mpg = miles / gallons
+
 # display result
 print('Your car gets an average of', format(mpg, ',.2f'), 'miles per gallon.')
