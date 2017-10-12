eaf98e0dc3ab6f5684eb0bb0294cbe8e8a5368ad
Evan Douglass <evanldouglass@gmail.com>
2017-10-11 21:02:10 -0700

    Celsius-to-Fahrenheit

---
+++ Starter_Programs/Celsius-to-Fahrenheit-Converter.py
@@ -0,0 +1,13 @@
+# Evan Douglass
+# Celsius to Fahrenheit
+
+# This program converts temperature in celsius to fahrenheit.
+
+# input temp in C
+C = int(input('Temperature in degrees celsius: '))
+
+# convert C into F
+F = int((9/5) * C + 32)
+
+# display conversion
+print('Temperature in degrees fahrenheit:', F)
